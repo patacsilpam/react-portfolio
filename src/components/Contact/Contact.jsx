24e6e8c07192b828750080182566ce5d6a436eaa
Pam Patacsil <patacsilpamela.bsit@gmail.com>
2023-02-06 06:51:36 +0800

    added contacts component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faFacebookF,faInstagram ,faTwitter,faLinkedin,faGithub } from '@fortawesome/free-brands-svg-icons';
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className='contact'>
+      <h1>Get in touch</h1>
+      <div className='contact-section'>
+        <div>
+          <p>Currently, I am looking for opportunities and accepting freelance projects. 
+            If you think I am a good asset, kindly send me a message.
+          </p>
+          <a href="mailto:pammpatacsil@gmail.com" >pammpatacsil@gmail.com</a>
+        </div>
+        <div className="soc-med">
+          <FontAwesomeIcon icon={faFacebookF} />
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon icon={faGithub} />
+          <a href="#">© 2023 Pamela M. Patacsil. All Rights Reserved</a>
+        </div>
+      </div>
+    </section>
   )
 }
