6c9e46352bb67fe876651076f462a0d261c82651
Pam Patacsil <patacsilpamela.bsit@gmail.com>
2023-02-07 07:04:57 +0800

    git added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 
 import "./App.css"
 import { NavBar } from "./components/NavBar"
+import { Home } from "./components/Home/Home";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Projects } from "./components/Task/Projects";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate
-} from "react-router-dom";
-
+import { Skills } from "./components/About/Skills";
+import { Footer } from "./components/Footer"
 function App() {
-
   return (
-    <div className="body" >
-     <NavBar/>
-    
+    <div className="body">
+    <NavBar/>
     <div className="route-con">
-      <Routes>
-          <Route path="/" element={<About/>}/>
-          <Route path="/projects" element={<Projects/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-      </Routes>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
     </div>
   )
