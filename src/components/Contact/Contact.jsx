5e695edbdee18cc356d6a8f8e7a9e868ba1fb9fe
Pam Patacsil <patacsilpamela.bsit@gmail.com>
2023-02-09 06:30:12 +0800

    refactor code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,24 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {  faFacebookF,faInstagram ,faTwitter,faLinkedin,faGithub } from '@fortawesome/free-brands-svg-icons';
 export const Contact = () => {
   return (
-    <section className='contact'>
-      <h1>Get in touch</h1>
-      <div className='contact-section'>
+    <section className='contact' id='contact'>
         <div>
-          <p>Currently, I am looking for opportunities and accepting freelance projects. 
-            If you think I am a good asset, kindly send me a message.
-          </p>
-          <a href="mailto:pammpatacsil@gmail.com" >pammpatacsil@gmail.com</a>
+            <h1>Get in touch</h1>
+            <p>Currently, I am looking for opportunities and accepting freelance projects. 
+              If you think I am a good asset, kindly send me a message.
+            </p><br />
+            <a href="mailto:pammpatacsil@gmail.com" className='email'>pammpatacsil@gmail.com</a>
         </div>
-        <div className="soc-med">
-          <FontAwesomeIcon icon={faFacebookF} />
-          <FontAwesomeIcon icon={faInstagram} />
-          <FontAwesomeIcon icon={faTwitter} />
-          <FontAwesomeIcon icon={faLinkedin} />
-          <FontAwesomeIcon icon={faGithub} />
-          <a href="#">© 2023 Pamela M. Patacsil. All Rights Reserved</a>
-        </div>
-      </div>
     </section>
   )
 }
