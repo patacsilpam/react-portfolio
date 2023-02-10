727cbb984e590ba0257259f889db1fb0a8259256
Pam Patacsil <patacsilpamela.bsit@gmail.com>
2023-02-10 20:53:42 +0800

    refactor code

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import svg from '../../assets/react.svg'
+import projects from '../../projects.json';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faArrowRightLong, faArrowUp, faArrowUpRightFromSquare, faFolderBlank, faSquareArrowUpRight } from '@fortawesome/free-solid-svg-icons';
 
 export const Task = () => {
   return (
     <section className='project' id='projects'>
       <h1>Projects</h1>
       <div className="project-section">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
+          {projects.map(project => {
+            return (
+              <div key={project.id}>
+                  <img src={project.img} />
+                  <div className="img-overlay">
+                    <div>
+                      <a href={project.link} target={'_blank'}><FontAwesomeIcon icon={faArrowUpRightFromSquare}  /></a>
+                      <a href={project.github_link} target={'_blank'}><FontAwesomeIcon icon={faGithub} /></a>
+                    </div>
+                    <h1>{project.title}</h1>
+                    <p>{project.description}</p>
+                    <small>{project.tools}</small>
+                  </div>
+              </div>
+            )
+          })}
       </div>
     </section>
   )
