3602c538fc67da6669f432fc29b8b09ea1226a71
Pam Patacsil <90546802+yskooo@users.noreply.github.com>
2023-02-13 06:17:41 +0800

    installed gsap

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
+import React,{useRef,useEffect} from 'react'
 import resume from '../../assets/pdf/resume.pdf'
-import { About } from '../About/About'
-export const Home = () => {
+import gsap from 'gsap'
+export const Home = () => { 
+  const helloRef = useRef(null);
+  useEffect(()=>{
+    const elHello = helloRef.current;
+    gsap.fromTo(elHello,{rotation:0},{rotation:-20,duration:3})
+  },[])
   return (
       <section className='home' id='home'>
           <div>
-            <span>&#128075; Hello,</span>
+            <span ref={helloRef}>&#128075; Hello,</span>
             <h1>I'm <span>Pam Patacsil</span></h1>
             <p>I specialize in building web & mobile applications based in Philippines.</p>
             <div><br />
